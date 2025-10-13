8c7d1865c3db7c61d1986263892dd13b22baf0ba
ericthecurious <hello@ericthecurious.com>
2025-10-13 12:25:43 -0700

    patch: add tracking for optional options to fakeWriteFile

--- src/testDoubles/fs-promises/fakeWriteFile.ts
+++ src/testDoubles/fs-promises/fakeWriteFile.ts
@@ -1,12 +1,17 @@
 export let callsToWriteFile: {
     file: string
     data: string
+    options?: Record<string, unknown>
 }[] = []
 
 export function resetCallsToWriteFile() {
     callsToWriteFile = []
 }
 
-export default async function fakeWriteFile(file: string, data: string) {
-    callsToWriteFile.push({ file, data })
+export default async function fakeWriteFile(
+    file: string,
+    data: string,
+    options?: Record<string, unknown>
+) {
+    callsToWriteFile.push({ file, data, options })
 }
