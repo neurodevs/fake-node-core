323384bf2eb01347d09eebff8b97ba35cc59abc5
ericthecurious <hello@ericthecurious.com>
2025-10-17 11:32:38 -0700

    minor: add more util functions for fakeExec

--- src/testDoubles/child_process/fakeExec.ts
+++ src/testDoubles/child_process/fakeExec.ts
@@ -6,13 +6,19 @@ export function resetCallsToExec() {
     callsToExec = []
 }
 
+export const defaultExecResult = { stdout: '', stderr: '' }
+
 export let fakeExecResult: Record<string, ChildProcess> = {}
 
 export function setFakeExecResult(command: string, result: ChildProcess) {
     fakeExecResult[command] = result
 }
 
+export let resetFakeExecResults = () => {
+    fakeExecResult = {}
+}
+
 export default async function fakeExec(command: string) {
     callsToExec.push(command)
-    return fakeExecResult[command]
+    return fakeExecResult[command] ?? defaultExecResult
 }
