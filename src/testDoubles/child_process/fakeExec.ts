0daee2ea3b532286c2277749d2879291f4425706
ericthecurious <hello@ericthecurious.com>
2025-12-20 09:50:03 -0800

    minor: create mechanism for exec to throw

--- src/testDoubles/child_process/fakeExec.ts
+++ src/testDoubles/child_process/fakeExec.ts
@@ -21,10 +21,21 @@ export let resetFakeExecResults = () => {
     fakeExecResult = {}
 }
 
+export const shouldThrowFor = new Set<string>()
+
+export function setShouldThrowFor(command: string) {
+    shouldThrowFor.add(command)
+}
+
 export default async function fakeExec(
     command: string,
     options?: Record<string, unknown>
 ) {
     callsToExec.push({ command, options })
+
+    if (shouldThrowFor.has(command)) {
+        throw new Error(`Fake exec error for command: ${command}`)
+    }
+
     return fakeExecResult[command] ?? defaultExecResult
 }
